f57e01628b8e2f245f0fa9d1ac2866b36e1a7b25
Andrew Van Beek <andrewvanbeek90gmail.com>
2016-11-25 12:16:35 -0600

    adds jquery and header

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,18 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
-
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  $( "#lglink" ).on("mouseenter", function(e) {
+    e.preventDefault();
+    $("#logout").fadeToggle();
+  });
+   $( "#loggingout" ).on("mouseleave", function(e) {
+    e.preventDefault();
+    $("#logout").hide();
+  });
+     $( "#lginlink" ).on("mouseenter", function(e) {
+    e.preventDefault();
+    $("#login").fadeToggle();
+  });
+   $( "#loggingin" ).on("mouseleave", function(e) {
+    e.preventDefault();
+    $("#login").hide();
+  });
 });
